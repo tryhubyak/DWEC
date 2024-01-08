f62b5ab97ddd839bb3f52ca9fe8929a580cdfdb7
Veronika Tryhubyak <vtr0003@alu.medac.es>
2024-01-08 22:56:39 +0100

    [Merch | New] - items sacados de json y motor de busqueda
    Items ahora estan por json y ademas motor de busqueda que va filtrando las prendas que coincidan con la palabra borrando el resto que no coincidan, falta filtrado por categorias

---
+++ Practica 5 - JQUERY/js/merch.js
@@ -0,0 +1,101 @@
+//Carousel slick de merch
+function discoverL() {
+  $(".center").slick({
+      dots: true,
+      infinite: true,
+      centerMode: true,
+      slidesToShow: 3,
+      slidesToScroll: 1
+  });
+}
+
+function discoverM() {
+  $(".center").slick({
+      dots: true,
+      infinite: true,
+      centerMode: false,
+      slidesToShow: 2,
+      slidesToScroll: 1
+  });
+}
+
+function discoverS() {
+  $(".center").slick({
+      dots: true,
+      infinite: true,
+      centerMode: true,
+      slidesToShow: 1,
+      slidesToScroll: 1
+  });
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  discoverL();
+});
+
+if (window.matchMedia("(width: 768px) and (height: 1024px)").matches) {
+  discoverM();
+}
+if (window.matchMedia("(width: 390px) and (height: 844px)").matches) {
+  discoverS();
+}
+
+//seccion MERCH
+$.ajax({
+  url: '../json/all.json',
+  method: 'GET',
+  dataType: 'json',
+  success: function (data) {
+    var dataRandom = shuffleArray(data);
+    itemsHTML(dataRandom);
+
+
+    $('.buscar input').on('input', function () {
+      var searchTerm = $(this).val().toLowerCase();
+      filterItems(dataRandom, searchTerm);
+    });
+  },
+  error: function (error) {
+    console.error('Error en la solicitud:', error);
+  }
+});
+
+function itemsHTML(data) {
+  var contenedorall = $('#contenedorall');
+  contenedorall.empty();
+
+  data.forEach(function (all) {
+    var allHTML = `
+      <div class="col-12 col-md-6 col-lg-4">
+        <div class="card mb-2">
+          <img src="${all.foto}" alt="${all.nombre}" class="tienda card-img-top">
+          <div class="card-body">
+            <h3 class="card-title">${all.nombre}</h3>
+            <p class="card-text">$${all.precio}</p>
+          </div>
+        </div>
+      </div>
+    `;
+    contenedorall.append(allHTML);
+  });
+}
+
+function filterItems(data, searchTerm) {
+  var contenedorall = $('#contenedorall');
+  contenedorall.empty();
+
+  var filteredData = data.filter(function (all) {
+    return all.nombre.toLowerCase().includes(searchTerm);
+  });
+
+  itemsHTML(filteredData);
+}
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
